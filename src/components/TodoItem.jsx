21485ae7e6f0b414fc87121473d0ad713f5ec119
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-10 14:45:44 -0500

    feat: add check todo ui

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,31 +1,48 @@
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
-import { deleteTodo } from "@features/todos/todosSlice";
+import { checkTodo, deleteTodo } from "@features/todos/todosSlice";
 
 const TodoItem = ({ todo }) => {
 	const dispatch = useDispatch();
 	const handleDelete = (id) => {
 		dispatch(deleteTodo(id));
 	};
+	const handleCheck = (id) => {
+		dispatch(checkTodo(id));
+	};
 	return (
-		<li className="bg-neutral-800 my-2 p-3 rounded-lg flex justify-between items-center">
-			<p className={`${todo.completed && "line-through"}`}>
-				{todo.id} - {todo.title} : {todo?.description}
-			</p>
-			<div className="flex gap-4">
-				<Link to={`/edit-todo/${todo.id}`}>
-					<button className="bg-zinc-600 w-28 h-8 rounded-md hover:bg-zinc-700 text-gray-300 hover:text-gray-50">
-						Edit
-					</button>
-				</Link>
-				<button
-					className="bg-red-600 w-28 h-8 rounded-md hover:bg-red-700 text-gray-300 hover:text-gray-50"
-					onClick={() => handleDelete(todo.id)}
+		<li className="bg-neutral-800 my-2 p-3 rounded-lg grid grid-cols-[auto_1fr_auto_auto] items-center gap-4">
+			<input
+				type="checkbox"
+				checked={todo.completed}
+				onChange={() => handleCheck(todo.id)}
+			/>
+			<div>
+				<p
+					className={`${todo.completed && "line-through"} hover:cursor-pointer`}
+					onClick={() => handleCheck(todo.id)}
 				>
-					Delete
-				</button>
+					{todo.title}
+				</p>
+				<p
+					className={`${todo.completed && "line-through"} hover:cursor-pointer`}
+					onClick={() => handleCheck(todo.id)}
+				>
+					{todo?.description}
+				</p>
 			</div>
+			<Link to={`/edit-todo/${todo.id}`}>
+				<button className="bg-zinc-600 w-20 h-8 rounded-md hover:bg-zinc-700 text-gray-300 hover:text-gray-50">
+					Edit
+				</button>
+			</Link>
+			<button
+				className="bg-red-600 w-20 h-8 rounded-md hover:bg-red-700 text-gray-300 hover:text-gray-50"
+				onClick={() => handleDelete(todo.id)}
+			>
+				Delete
+			</button>
 		</li>
 	);
 };
