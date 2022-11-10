349a5d591320d1a7d650e88f1be2715bd742aea0
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-10 14:06:04 -0500

    feat: add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+	return (
+		<footer className="text-center py-3 fixed bg-zinc-800 bottom-0 w-full text-xs">
+			Coded with ❤️ by 
+			<a href="https://github.com/gustavoylc">
+				<strong> @gustavoylc</strong>
+			</a>
+		</footer>
+	);
+};
+
+export { Footer };
