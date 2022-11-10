cf7d3a6f3221ea208a81fecded98d66dc6f06115
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-10 14:00:31 -0500

    chore(vite): add aliases

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import * as path from "path";
 import { defineConfig } from "vite";
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
@@ -14,20 +15,8 @@ export default defineConfig({
 				replacement: path.resolve(__dirname, "src/components"),
 			},
 			{
-				find: "@actions",
-				replacement: path.resolve(__dirname, "src/redux/actions"),
-			},
-			{
-				find: "@middlewares",
-				replacement: path.resolve(__dirname, "src/redux/middlewares"),
-			},
-			{
-				find: "@reducers",
-				replacement: path.resolve(__dirname, "src/redux/reducers"),
-			},
-			{
-				find: "@store",
-				replacement: path.resolve(__dirname, "src/redux/store"),
+				find: "@app",
+				replacement: path.resolve(__dirname, "src/app"),
 			},
 			{
 				find: "@pages",
@@ -36,6 +25,10 @@ export default defineConfig({
 			{
 				find: "@services",
 				replacement: path.resolve(__dirname, "src/services"),
+			},
+			{
+				find: "@features",
+				replacement: path.resolve(__dirname, "src/features"),
 			},
 		],
 	},
