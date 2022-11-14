4a26581e562f9b555430c7b6953c2c0cc3e0c843
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-14 09:06:12 -0500

    feat: add Todos Page

---
+++ src/pages/Todos.jsx
@@ -0,0 +1,11 @@
+import { Header } from "@components/Header";
+import { Main } from "@components/Main";
+
+export const Todos = () => {
+	return (
+		<main className="grid grid-rows-[auto_fr_auto]">
+			<Header />
+			<Main />
+		</main>
+	);
+};
