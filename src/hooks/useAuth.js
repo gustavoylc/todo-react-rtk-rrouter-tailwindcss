f4f2d03c2579a9d98ccfa42adff6c5875a0279dd
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-14 15:09:09 -0500

    feat: add useAuth hook for login and logout

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+
+
+import { setUser } from "@features/users/usersSlice";
+
+
+const adminList = ["iris", "reta", "fred"];
+export const useAuth = () => {
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const user = useSelector((state) => state.usersData.user);
+	const login = ({username}) => {
+		const isAdmin = Boolean(adminList.find((admin) => admin === username));
+		dispatch(setUser({username, isAdmin}));
+		navigate("/profile");
+	};
+
+	const logout = () => {
+		dispatch(setUser(null));
+		navigate("/");
+	};
+
+	return { user, login, logout };
+};
