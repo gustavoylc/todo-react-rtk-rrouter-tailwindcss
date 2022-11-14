e2ae2c9bfcb72e7ba3d44a358a1dc1b9f15c3670
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-14 15:01:20 -0500

    style: change header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => {
 	const todosList = useSelector((state) => state.todosData.todos);
 	const completedTodos = todosList.filter((todo) => todo.completed).length;
 	return (
-		<header className="text-center my-4">
-			<h3>
+		<header className="text-center my-4 mt-24 ">
+			<h4>
 				Has completado {completedTodos} de {todosList.length} Todos
-			</h3>
+			</h4>
 			<Searcher />
 		</header>
 	);
