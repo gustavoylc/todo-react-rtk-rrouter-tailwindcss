32681dbba239a274b7bb7e6261c0582df7b3d96f
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-14 09:04:56 -0500

    feat: add uiTodosReducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import  todosReducer  from "@features/todos/todosSlice";
+import todosReducer from "@features/todos/todosSlice";
+import uiTodosReducer from "@features/todos/uiSlice";
 
 export const store = configureStore({
-	reducer: {
-		todosData: todosReducer,
-	},
+	reducer: { todosData: todosReducer, todosUi: uiTodosReducer },
 });
