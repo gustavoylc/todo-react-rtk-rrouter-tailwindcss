0547b564cee0d6d7ef86185308585b905f4748c4
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-14 14:55:08 -0500

    feat: add usersData reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import todosReducer from "@features/todos/todosSlice";
 import uiTodosReducer from "@features/todos/uiSlice";
+import usersReducer from "@features/users/usersSlice";
 
 export const store = configureStore({
-	reducer: { todosData: todosReducer, todosUi: uiTodosReducer },
+	reducer: {
+		todosData: todosReducer,
+		todosUi: uiTodosReducer,
+		usersData: usersReducer,
+	},
 });
