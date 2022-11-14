4cf02cec5535c6350118de7aaf4d67981ddeb6d2
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-14 15:10:02 -0500

    refactor: change Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,25 @@
-import { Footer } from "@components/Footer";
-import { Header } from "@components/Header";
-import { Main } from "@components/Main";
-
 export const Home = () => {
 	return (
-		<main className="grid grid-rows-[auto_fr_auto]">
-			<Header />
-			<Main />
-			<Footer />
+		<main className="mt-24 px-5 leading-6 grid gap-4 font-Poppins font-extralight">
+			<h2 className="font-medium">Principal Information</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+				recusandae temporibus architecto. Laboriosam eveniet accusamus hic illo!
+				Sit expedita reprehenderit commodi nobis earum nesciunt dolorum corrupti
+				quidem. Tenetur, minus officiis!
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+				recusandae temporibus architecto. Laboriosam eveniet accusamus hic illo!
+				Sit expedita reprehenderit commodi nobis earum nesciunt dolorum corrupti
+				quidem. Tenetur, minus officiis!
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+				recusandae temporibus architecto. Laboriosam eveniet accusamus hic illo!
+				Sit expedita reprehenderit commodi nobis earum nesciunt dolorum corrupti
+				quidem. Tenetur, minus officiis!
+			</p>
 		</main>
 	);
 };
