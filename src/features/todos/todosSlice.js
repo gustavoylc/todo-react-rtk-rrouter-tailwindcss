7e200cadaedd71938c35d4ac52ba90da345904cf
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-14 09:08:01 -0500

    feat: adapt to dynamic data

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -1,55 +1,100 @@
-import { createSlice } from "@reduxjs/toolkit";
-import "@services/todos";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { addTodoService, deleteTodoService, editTodoService, getTodoListService, searchTodoService } from "@services/todos";
+
+
+
+import { setLoading } from "@features/todos/uiSlice";
 
 
 const initialState = {
-	todos: [
-		{
-			userId: 10,
-			id: 197,
-			title: "dignissimos quo nobis earum saepe",
-			description: "description 1",
-			completed: true,
-		},
-		{
-			userId: 10,
-			id: 198,
-			title: "quis eius est sint explicabo",
-			description: "description 2",
-			completed: true,
-		},
-	],
+	todos: [],
 };
+
+export const fetchGetTodoList = createAsyncThunk(
+	"todosList/fetchGetTodoList",
+	async (_, { dispatch }) => {
+		dispatch(setLoading(true));
+		const todoList = await getTodoListService();
+		dispatch(initialsTodos(todoList));
+		dispatch(setLoading(false));
+	}
+);
+
+export const fetchAddTodo = createAsyncThunk(
+	"todosList/fetchAddTodo",
+	async (titleWithDescription, { dispatch }) => {
+		dispatch(setLoading(true));
+		const newTodo = await addTodoService(titleWithDescription);
+		dispatch(addTodo(newTodo));
+		dispatch(setLoading(false));
+	}
+);
+
+export const fetchEditTodo = createAsyncThunk(
+	"todosList/fetchEditTodo",
+	async (editTodoValues, { dispatch }) => {
+		dispatch(setLoading(true));
+		const editedTodo = await editTodoService(editTodoValues);
+		dispatch(editTodo(editedTodo));
+		dispatch(setLoading(false));
+	}
+);
+export const fetchDeleteTodo = createAsyncThunk(
+	"todosList/fetchDeleteTodo",
+	async (todoId, { dispatch }) => {
+		dispatch(setLoading(true));
+		const deletedTodo = await deleteTodoService(todoId);
+		dispatch(deleteTodo(deletedTodo));
+		dispatch(setLoading(false));
+	}
+);
+
+export const fetchSearchTodo = createAsyncThunk(
+	"todosList/fetchSearchTodo",
+	async (searchValue, { dispatch }) => {
+		dispatch(setLoading(true));
+		const foundTodo = await searchTodoService(searchValue);
+		dispatch(initialsTodos(foundTodo));
+		dispatch(setLoading(false));
+	}
+);
 
 export const todosSlice = createSlice({
 	name: "todosList",
 	initialState,
 	reducers: {
-		initialsTodos: (state, action) => ({
-			// todos: action.payload,
-		}),
-		addTodo: (state, action) => ({ todos: [...state.todos, action.payload] }),
+		initialsTodos: (state, action) => {
+			state.todos = action.payload;
+		},
+		addTodo: (state, action) => {
+			state.todos.push(action.payload);
+			// ({ todos: [...state.todos, action.payload] }),
+		}, 
 		editTodo: (state, action) => {
 			const foundTodo = state.todos.find(
 				(todo) => todo.id === action.payload.id
 			);
 			if (foundTodo) {
 				foundTodo.title = action.payload.title;
-				foundTodo.description = action.payload.description;
+				foundTodo.completed = action.payload.completed;
 			}
 		},
 		deleteTodo: (state, action) => {
 			const foundTodo = state.todos.find((todo) => todo.id === action.payload);
 			foundTodo && state.todos.splice(state.todos.indexOf(foundTodo), 1);
 		},
-		checkTodo: (state, action) => {
-			const foundTodo = state.todos.find((todo) => todo.id === action.payload);
-			foundTodo.completed = !foundTodo.completed;
-		},
+		searchTodo: (state, action) => ({
+			todos: action.payload,
+		}),
 	},
 });
 
-export const { initialsTodos, addTodo, deleteTodo, editTodo, checkTodo } =
-	todosSlice.actions;
+export const {
+	initialsTodos,
+	addTodo,
+	deleteTodo,
+	editTodo,
+	searchTodo,
+} = todosSlice.actions;
 
 export default todosSlice.reducer;
