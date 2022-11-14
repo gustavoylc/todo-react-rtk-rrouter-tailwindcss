4e65ba7d595072c4e69aba5813f479f5553562f5
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-14 15:10:59 -0500

    feat: add FAQ static data

---
+++ src/services/faqData.js
@@ -0,0 +1,24 @@
+const faqData = [];
+faqData.push({
+	title: "¿Qué es un todo?",
+	slug: "que-es-un-todo",
+	content:
+		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque recusandae temporibus architecto. Laboriosam eveniet accusamus hic illo! Sit expedita reprehenderit commodi nobis earum nesciunt dolorum corrupti quidem. Tenetur, minus officiis!",
+	author: "juan",
+});
+faqData.push({
+	title: "¿Por qué usar todos?",
+	slug: "por-que-usar-todos",
+	content:
+		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque recusandae temporibus architecto. Laboriosam eveniet accusamus hic illo! Sit expedita reprehenderit commodi nobis earum nesciunt dolorum corrupti quidem. Tenetur, minus officiis!",
+	author: "diana",
+});
+faqData.push({
+	title: "¿Cuantos todos puedo hacer?",
+	slug: "cuantos-todos-puedo-hacer",
+	content:
+		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque recusandae temporibus architecto. Laboriosam eveniet accusamus hic illo! Sit expedita reprehenderit commodi nobis earum nesciunt dolorum corrupti quidem. Tenetur, minus officiis!",
+	author: "josh",
+});
+
+export { faqData };
