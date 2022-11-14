3442bb6b76348b0ea55c8b49e3b30b33eaf807ee
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-14 15:03:00 -0500

    refactor: refactoring routes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,27 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import { TodoForm } from "@components/TodoForm";
 import { TodoList } from "@components/TodoList";
 
 const Main = () => {
 	return (
 		<main className="h-full flex justify-center ">
-			<BrowserRouter>
-				<Routes>
-					<Route
-						path="/"
-						element={<TodoList />}
-					/>
-					<Route
-						path="/create-todo"
-						element={<TodoForm />}
-					/>
-					<Route
-						path="/edit-todo/:id"
-						element={<TodoForm />}
-					/>
-				</Routes>
-			</BrowserRouter>
+			<TodoList />
 		</main>
 	);
 };
