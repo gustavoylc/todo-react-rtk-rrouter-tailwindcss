8c09981b6591aea472b8a5987054ccc346ff03ea
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-14 09:04:13 -0500

    feat: add endpoints with axios

--- src/services/todos.js
+++ src/services/todos.js
@@ -1,32 +1,70 @@
-export const getTodoList = () => {
-	return fetch("https://jsonplaceholder.typicode.com/todos")
-		.then((response) => response.json())
-		.then((response) => response)
-		.catch((err) => console.error(err));
+import axios from "axios";
+
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.post["Content-Type"] = "application/json; charset=UTF-8";
+axios.defaults.headers.put["Content-Type"] = "application/json; charset=UTF-8";
+
+export const getTodoListService = async () => {
+	try {
+		const params = {
+			params: {
+				_sort: "id",
+				_order: "desc",
+			},
+		};
+		const result = await axios.get("todos", params);
+		return result.data;
+	} catch (error) {
+		console.error(error);
+	}
 };
-// 
-export const createTodo = (title) => {
-	const options = {
-		method: "POST",
-		body: JSON.stringify({
-			title: `${title}`,
+
+export const addTodoService = async (title) => {
+	try {
+		const data = {
+			title,
 			completed: false,
 			userId: 1,
-		}),
-		headers: {
-			"Content-type": "application/json; charset=UTF-8",
-		},
-	};
-	return fetch("https://jsonplaceholder.typicode.com/todos", options)
-		.then((response) => response.json())
-		.then((response) => response)
-		.catch((err) => console.error(err));
+		};
+		const result = await axios.post("todos", data);
+		return result.data;
+	} catch (error) {
+		console.error(error);
+	}
 };
 
+export const searchTodoService = async (searchValue) => {
+	try {
+		const params = {
+			params: {
+				q: searchValue,
+			},
+		};
+		const result = await axios.get("todos", params);
+		return result.data;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
-export const getSearch = (param) => {
-	return fetch(`https://jsonplaceholder.typicode.com/todos?q=${param}`)
-		.then((response) => response.json())
-		.then((response) => response)
-		.catch((err) => console.error(err));
+export const editTodoService = async (editTodo) => {
+	try {
+		const data = {
+			title: editTodo.title,
+			completed: editTodo.completed,
+		};
+		const result = await axios.patch("todos/" + editTodo.id, data);
+		return result.data;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export const deleteTodoService = async (todoId) => {
+	try {
+		const result = await axios.delete("todos/" + todoId);
+		if (result.status === 200) return todoId;
+	} catch (error) {
+		console.error(error);
+	}
 };
