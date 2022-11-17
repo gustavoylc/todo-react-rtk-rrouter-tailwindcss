bd8cf032238d6089335b0fa841b891618e3aca5b
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-17 15:17:35 -0500

    feat: add router config

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import "@styles/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { RouterProvider } from "react-router-dom";
-import { router } from "@router";
 
 import { store } from "@app/store";
+
+import { App } from "./App";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<RouterProvider router={router} />
+			<App />
 		</Provider>
 	</React.StrictMode>
 );
