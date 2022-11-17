e20946bd68a333fd0f40940b69c2a2fadbf11609
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-17 15:24:46 -0500

    refactor: formatted code

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,19 +1,16 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-
-
 import { setUser } from "@features/users/usersSlice";
-
 
 const adminList = ["iris", "reta", "fred"];
 export const useAuth = () => {
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
 	const user = useSelector((state) => state.usersData.user);
-	const login = ({username}) => {
+	const login = ({ username }) => {
 		const isAdmin = Boolean(adminList.find((admin) => admin === username));
-		dispatch(setUser({username, isAdmin}));
+		dispatch(setUser({ username, isAdmin }));
 		navigate("/profile");
 	};
 
