d821d90c9c97660a17e378f5ddeb7807130a882a
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2022-11-17 15:25:17 -0500

    refactor: deleting unused code

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,14 +1,9 @@
-import { useRouteError } from "react-router-dom";
-
 const NotFound = () => {
-	const error = useRouteError();
-	console.log(error);
-
 	return (
 		<div className="mt-24 px-5">
 			<h1>404</h1>
+
 			<p>Page not found</p>
-			<p>{error.statusText || error.message}</p>
 		</div>
 	);
 };
